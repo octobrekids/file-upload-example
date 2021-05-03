44478b28ef2f684a322ed37891ae0a62699a1f63
octobrekids <chnrd.k@gmail.com>
2021-05-04 01:52:53 +0700

    add input multiple file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { uploadActions } from './reducers/uploadFileReducer';
+import { useDispatch } from 'react-redux';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const dispatch = useDispatch();
+	const handleAttachFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+		console.log('files upload', e.target.files);
+		if (e.target.files) dispatch(uploadActions.setUploadFile(e.target.files));
+		e.target.value = '';
+	};
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<input type="file" multiple onChange={handleAttachFile} />
+			</header>
+		</div>
+	);
 }
 
 export default App;
