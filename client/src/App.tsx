d223575154f959f513b6657c6b372bff5d72f3bb
octobrekids <chnrd.k@gmail.com>
2021-05-04 02:26:29 +0700

    select upload file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { uploadActions } from './reducers/uploadFileReducer';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import UploadProgress from './components/UploadProgress';
+import { StoresState } from './store';
 
 function App() {
 	const dispatch = useDispatch();
+	const UploadFile = useSelector(
+		(state: StoresState) => state.upload.data.fileProgress
+	);
 	const handleAttachFile = (e: React.ChangeEvent<HTMLInputElement>) => {
 		console.log('files upload', e.target.files);
 		if (e.target.files) dispatch(uploadActions.setUploadFile(e.target.files));
@@ -17,6 +22,7 @@ function App() {
 				<img src={logo} className="App-logo" alt="logo" />
 				<input type="file" multiple onChange={handleAttachFile} />
 			</header>
+			<UploadProgress fileProgress={UploadFile} />
 		</div>
 	);
 }
