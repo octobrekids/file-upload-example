0ffdd607a0e83c45a97c660c482df1351aa5af6e
octobrekids <chnrd.k@gmail.com>
2021-05-04 02:13:16 +0700

    add WrapperItem style

---
+++ client/src/components/UploadProgress/UploadItem/styled.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const WrapperItem = styled.div`
+	display: grid;
+	grid-template-columns: calc(100% * 50px) 50px;
+	padding: 16px;
+	box-shadow: 0px 1px 2px lightgray;
+`;
+
+export { WrapperItem };
