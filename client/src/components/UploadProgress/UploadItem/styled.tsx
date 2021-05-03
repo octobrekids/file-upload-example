ad5aaa7f0918da75e2aa0b193f2d6ffe04ec9232
octobrekids <chnrd.k@gmail.com>
2021-05-04 02:16:13 +0700

    add progressBar style

--- client/src/components/UploadProgress/UploadItem/styled.tsx
+++ client/src/components/UploadProgress/UploadItem/styled.tsx
@@ -14,4 +14,18 @@ const LeftSide = styled.div`
 	margin-right: 8px;
 `;
 
+const progressBar = styled.div`
+	width: 100%;
+	height: 15px;
+	background-color: lightgray;
+	margin-bottom: 8px;
+	border-radius: 20px;
+
+	div {
+		height: 15px;
+		background-color: lightgreen;
+		border-radius: 20px;
+	}
+`;
+
 export { WrapperItem, LeftSide };
